4ad28a57e972d296b9df60180ea961b8ae996a99
pedrohls10 <pedro.santos@bandtec.com.br>
2021-05-31 21:52:55 -0300

    Site

---
+++ API-projeto-site/models/usuario.js
@@ -0,0 +1,45 @@
+	'use strict';
+
+/* 
+lista e explicação dos Datatypes:
+https://codewithhugo.com/sequelize-data-types-a-practical-guide/
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    let Usuario = sequelize.define('Usuario',{
+		id: {
+			field: 'idUsuario',
+			type: DataTypes.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},		
+		nome: {
+			field: 'nomeUsuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		login: {
+			field: 'usuario',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		senha: {
+			field: 'senha',
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		fkTime: {
+			field: 'fkTime',
+			type: DataTypes.STRING,
+			allowNull: false
+		}
+	},
+	{
+		tableName: 'usuario', 
+		freezeTableName: true, 
+		underscored: true,
+		timestamps: false,
+	});
+
+    return Usuario;
+};
